e576a70c814dd1f7568ae1820ecc1601a3197b12
Andrei Trifan <andreitrifan2003.16@gmail.com>
2025-05-03 14:36:56 +0000

    Update API documentation and handler routes for consistency; standardize endpoint naming conventions

--- service/api/api-handler.go
+++ service/api/api-handler.go
@@ -10,25 +10,25 @@ func (rt *_router) Handler() http.Handler {
 	//rt.router.GET("/", rt.wrap(rt.insertUser))
 	rt.router.GET("/context", rt.wrap(rt.getContextReply))
 
-	rt.router.POST("/session", rt.loginUser)
+	rt.router.POST("/session", rt.loginUser) //
 	rt.router.GET("/users/:user_id", rt.getUser)
-	rt.router.PUT("/settings/changename", rt.setUsername)
-	rt.router.PUT("/settings/changephoto", rt.setPhoto)
+	rt.router.PUT("/settings/changename", rt.setUsername) //
+	rt.router.PUT("/settings/changephoto", rt.setPhoto)   //
 	rt.router.GET("/users", rt.getAllUsers)
 
-	rt.router.GET("/chats/:chat_id", rt.getChat)
-	rt.router.GET("/chats", rt.getAllChats)
-	rt.router.POST("/createchat", rt.createChat)
-	rt.router.PUT("/chats/:chat_id/settings/changename", rt.setChatName)
-	rt.router.PUT("/chats/:chat_id/settings/changephoto", rt.setChatPhoto)
-	rt.router.POST("/chats/:chat_id/settings/add", rt.addUserToChat)
-	rt.router.DELETE("/chats/:chat_id/settings/leave", rt.leaveChat)
+	rt.router.GET("/chats/:chat_id", rt.getChat)                           //
+	rt.router.GET("/chats", rt.getAllChats)                                //
+	rt.router.POST("/createchat", rt.createChat)                           //
+	rt.router.PUT("/chats/:chat_id/settings/changename", rt.setChatName)   //
+	rt.router.PUT("/chats/:chat_id/settings/changephoto", rt.setChatPhoto) //
+	rt.router.POST("/chats/:chat_id/settings/add", rt.addUserToChat)       //
+	rt.router.DELETE("/chats/:chat_id/settings/leave", rt.leaveChat)       //
 
-	rt.router.POST("/chats/:chat_id", rt.sendMessage)
-	rt.router.POST("/chats/:chat_id/messages/:message_id", rt.forwardMessage)
-	rt.router.DELETE("/chats/:chat_id/messages/:message_id/delete", rt.deleteMessage)
-	rt.router.POST("/chats/:chat_id/messages/:message_id/reactions", rt.addReaction)
-	rt.router.DELETE("/chats/:chat_id/messages/:message_id/reactions/:reaction_id", rt.deleteReaction)
+	rt.router.POST("/chats/:chat_id", rt.sendMessage)                                                  //
+	rt.router.POST("/chats/:chat_id/messages/:message_id", rt.forwardMessage)                          //
+	rt.router.DELETE("/chats/:chat_id/messages/:message_id/delete", rt.deleteMessage)                  //
+	rt.router.POST("/chats/:chat_id/messages/:message_id/reactions", rt.addReaction)                   //
+	rt.router.DELETE("/chats/:chat_id/messages/:message_id/reactions/:reaction_id", rt.deleteReaction) //
 
 	// Special routes
 	rt.router.GET("/liveness", rt.liveness)
